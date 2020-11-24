f79d218b8b37de23e6051b813688f025ea9b95c0
Jesse Roland <j.roland277@gmail.com>
2020-11-24 14:12:40 -0600

    Use Alphanumeric Strings instead of SHA hashes for secret URL
    It's also configurable now
    
    Implements #8

--- lib/quick-secrets/secret/manager.rb
+++ lib/quick-secrets/secret/manager.rb
@@ -1,4 +1,5 @@
 require 'quick-secrets'
+require 'securerandom'
 require 'digest'
 
 # Manager to hold all encrypted secrets in memory...
@@ -6,6 +7,7 @@ module QuickSecrets
   module Secret
     class Manager
       
+      AUTH_UUID_LEN_DEFAULT = 8
 
       def initialize
         @secrets = {}
@@ -19,8 +21,28 @@ module QuickSecrets
         @secrets.keys
       end
 
-      def exists?(digest)
-        @secrets.key? digest
+      def exists?(uuid)
+        @secrets.key? uuid
+      end
+
+      # get the length for secret UUID's used in URL's
+      def uuid_len
+        len = QuickSecrets::Core.core.config["secret_uuid_length"].to_i
+        if len.nil?
+          AUTH_UUID_LEN_DEFAULT
+        else
+          len
+        end
+      end
+
+      # Generate a UUID for the secret's URL
+      def gen_uuid
+        new_uuid = nil
+        loop do
+          new_uuid = SecureRandom.alphanumeric(uuid_len)
+        break unless exists?(new_uuid)
+        end
+        new_uuid
       end
       
       # Stores a secret
@@ -31,19 +53,20 @@ module QuickSecrets
         cipher.iv = iv
         cipher.key = Digest::SHA256.digest password
         encrypted = cipher.update(secret)+cipher.final
-        key = Digest::SHA256.hexdigest encrypted
-        @secrets[key] = {iv: iv, enc: encrypted}
-        return key
+        #key = Digest::SHA256.hexdigest encrypted
+        secret_uuid = gen_uuid()
+        @secrets[secret_uuid] = {iv: iv, enc: encrypted}
+        return secret_uuid
       end
 
-      # Destroys it
-      def destroy(digest)
-        @secrets.delete(digest)
+      # Destroys a secret
+      def destroy(uuid)
+        @secrets.delete(uuid)
       end
       
-
-      def retrieve(digest, password)
-        if (data = @secrets[digest]).nil?
+      
+      def retrieve(uuid, password)
+        if (data = @secrets[uuid]).nil?
           return nil
         else
           cipher = OpenSSL::Cipher.new(crypt_method)
